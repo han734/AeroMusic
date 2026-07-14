23e3cbfcadac4e7ad54d5613837c7856495f80f5
han734 <shiyamgovind20041305@gmail.com>
2026-07-14 20:56:54 +0530

    fix: revert Android Studio native splash screen overrides and patch globules-io background mode auto-battery optimization call

--- android/app/src/main/java/com/aeromusic/premium/MainActivity.java
+++ android/app/src/main/java/com/aeromusic/premium/MainActivity.java
@@ -1,14 +1,5 @@
 package com.aeromusic.premium;
 
-import android.os.Bundle;
-import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // Handle the splash screen transition as per androidx.core:core-splashscreen
-        SplashScreen.installSplashScreen(this);
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
